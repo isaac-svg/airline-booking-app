2565bf8bff8aad68313ba522de0474124e62c5dc
isaac-svg <isaacsakyi009@gmail.com>
2024-03-27 19:50:56 +0000

    productionphase

--- src/services/gettabledata.js
+++ src/services/gettabledata.js
@@ -1,6 +1,18 @@
+import { isAuthorized } from "./getprofile";
 import { displayLinkedList } from "./writetable";
 
 export const getTableData = async () => {
+  isAuthorized()
+    .then((data) => {
+      if (!data) {
+        window.location.href =
+          "https://airticket-booking-app-orcin.vercel.app/src/pages/login.html";
+        return;
+      }
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
   try {
     const response = await fetch(
       "https://airticket-booking-app-orcin.vercel.app/all"
