85c7ccb82167dd8b8a01d65abc3d3194c5ba68a4
isaac-svg <isaacsakyi009@gmail.com>
2024-03-27 19:37:56 +0000

    productionphase

--- src/services/getprofile.js
+++ src/services/getprofile.js
@@ -1,13 +1,19 @@
 export const isAuthorized = async () => {
-  const response = await fetch(
-    "https://airticket-booking-app-orcin.vercel.app/auth/profile",
-    {
-      credentials: "include",
-    }
-  );
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      "https://airticket-booking-app-orcin.vercel.app/auth/profile",
+      {
+        credentials: "include",
+      }
+    );
+    const data = await response.json();
 
-  console.log(data, "data");
-  return data.success;
+    console.log(data, "data");
+    return data.success;
+  } catch (error) {
+    console.log(error);
+    window.location.href =
+      "https://airticket-booking-app-orcin.vercel.app/src/pages/book.html";
+  }
 };
 isAuthorized();
